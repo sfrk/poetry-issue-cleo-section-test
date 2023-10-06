27e46715251ddfe7f3864a713a53768dd16150e5
Sebastian Frelle Koch <sfrk@kamstrup.com>
2023-10-06 13:17:38 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from cleo.application import Application
+from cleo.commands.command import Command
+from cleo.helpers import argument, option
+
+class GreetCommand(Command):
+    name = "greet"
+    description = "Greets someone"
+    arguments = [
+        argument(
+            "name",
+            description="Who do you want to greet?",
+            optional=True
+        )
+    ]
+    options = [
+        option(
+            "yell",
+            "y",
+            description="If set, the task will yell in uppercase letters",
+            flag=True
+        )
+    ]
+
+    def handle(self):
+        name = self.argument("name")
+
+        if name:
+            text = f"Hello {name}"
+        else:
+            text = "Hello"
+
+        section = self._io.section()
+        section.write_line("<fg=blue>lul</>")
+
+        if self.option("yell"):
+            text = text.upper()
+
+        self.line(text)
+
+app = Application()
+app.add(GreetCommand())
+
+if __name__ == "__main__":
+    app.run()
